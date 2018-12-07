eb16418271b712ebba1bddd033a1f2770be679ac
Yakov Lipkovich <no@no>
2018-12-07 20:34:49 +0000

    Ported 2016-13 part 1 solution to A*

--- src/main/kotlin/ca/yakov/aoc/aoc2016/p13/consolidated.kt
+++ src/main/kotlin/ca/yakov/aoc/aoc2016/p13/consolidated.kt
@@ -2,36 +2,34 @@ package ca.yakov.aoc.aoc2016.p13
 
 import java.util.LinkedList
 
-data class Coord(val x: Int, val y: Int)
 class Node(val coord: Coord, val dist: Int)
-val spaces = mutableMapOf<Coord, Boolean>()
 
+/**
+ * Consolidated part 1 & 2 solver
+ */
 fun main(args: Array<String>) {
-    val visited = mutableSetOf(Coord(1, 1))
+    val closedSet = mutableSetOf(start)
+    val openMap = mutableMapOf<Coord, Stats>()
     val queue = LinkedList<Node>().also{ it.add(Node(Coord(1, 1), 0)) }
 
     while(!queue.isEmpty()) {
         val node = queue.remove()
         val coord = node.coord
-        visited.add(coord)
+        closedSet.add(coord)
         if(node.dist == 50) {
+            openMap += coord to Stats(coord, 50)
             continue
         }
 
         listOf(Coord(coord.x-1, coord.y), Coord(coord.x+1, coord.y), Coord(coord.x, coord.y-1), Coord(coord.x, coord.y+1))
-                .filter{ it !in visited && it.x >= 0 && it.y >= 0 && isSpace(it) }
+                .filter{ it !in closedSet && it.x >= 0 && it.y >= 0 && isSpace(it) }
                 .forEach{
-                    visited += it
+                    closedSet += it
                     queue += Node(it, node.dist+1)
                 }
     }
 
-    println(visited.size)
+    val visited = closedSet.size
+    aStar(openMap, closedSet)
+    println(visited)
 }
-
-fun isSpace(coord: Coord) =
-        spaces.getOrPut(coord) {
-            Integer
-                    .toBinaryString(coord.x*coord.x + 3*coord.x + 2*coord.x*coord.y + coord.y + coord.y*coord.y + input)
-                    .count{ it == '1' } % 2 == 0
-        }
--- src/main/kotlin/ca/yakov/aoc/aoc2016/p13/input.kt
+++ src/main/kotlin/ca/yakov/aoc/aoc2016/p13/input.kt
@@ -1,3 +1,21 @@
 package ca.yakov.aoc.aoc2016.p13
 
-val input = 1352
+import kotlin.math.abs
+
+const val input = 1352
+
+data class Coord(val x: Int, val y: Int)
+val start = Coord(1, 1)
+val goal = Coord(31, 39)
+
+class Stats(coord: Coord, val gScore: Int) {
+    val fScore = gScore + abs(coord.x-goal.x) + abs(coord.y-goal.y)
+}
+
+val spaces = mutableMapOf<Coord, Boolean>()
+fun isSpace(coord: Coord) =
+        spaces.getOrPut(coord) {
+            Integer
+                    .toBinaryString(coord.x*coord.x + 3*coord.x + 2*coord.x*coord.y + coord.y + coord.y*coord.y + input)
+                    .count{ it == '1' } % 2 == 0
+        }
