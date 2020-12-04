b2a727ab04c326dadf0e74362dcb333b95330559
wjykk <no@no>
2020-12-03 22:01:24 -0700

    2020 day 3 in Zig

---
+++ src/main/zig/2020/day03.zig
@@ -0,0 +1,31 @@
+const aoc = @import("../aoc.zig");
+const std = @import("std");
+
+pub fn run(problem: *aoc.Problem) !aoc.Solution {
+    var tree_list = std.ArrayList([]const u8).init(problem.allocator);
+    defer tree_list.deinit();
+    while (problem.line()) |line| {
+        try tree_list.append(line);
+    }
+    const trees = tree_list.items;
+
+    const res1 = countTrees(trees, 3, 1);
+    const res2 = countTrees(trees, 1, 1) * res1 * countTrees(trees, 5, 1) * countTrees(trees, 7, 1) * countTrees(trees, 1, 2);
+
+    return aoc.Solution { .p1 = res1, .p2 = res2 };
+}
+
+fn countTrees(trees: []const []const u8, right: u8, down: u8) usize {
+    var hit: usize = 0;
+    var row: usize = down;
+    var col: usize = right;
+
+    while (row < trees.len) : (row += down) {
+        if (trees[row][col] == '#') {
+            hit += 1;
+        }
+        col = (col + right) % trees[row].len;
+    }
+
+    return hit;
+}
--- src/main/zig/2020/runner.zig
+++ src/main/zig/2020/runner.zig
@@ -4,6 +4,7 @@ pub fn run(problem: *aoc.Problem, day: u16) !aoc.Solution {
     return try switch (day) {
         1 => @import("day01.zig").run(problem),
         2 => @import("day02.zig").run(problem),
+        3 => @import("day03.zig").run(problem),
         else => unreachable,
     };
 }
