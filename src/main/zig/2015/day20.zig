ede8e8a018b551a21dd0a1bc0cd467a4dade3a55
wjykk <no@no>
2020-12-07 19:02:02 -0700

    2015 day 20 in Zig

---
+++ src/main/zig/2015/day20.zig
@@ -0,0 +1,41 @@
+const aoc = @import("../aoc.zig");
+const std = @import("std");
+
+pub fn run(problem: *aoc.Problem) !aoc.Solution {
+    const input = try std.fmt.parseInt(usize, std.mem.trim(u8, problem.input, &std.ascii.spaces), 10);
+    var res1: ?usize = null;
+    var res2: ?usize = null;
+
+    var house: usize = 1;
+    while (res1 == null or res2 == null) : (house += 1) {
+        var factor1_sum: usize = 0;
+        var factor2_sum: usize = 0;
+
+        var i: usize = 1;
+        const max = std.math.sqrt(house);
+        while (i <= max) : (i += 1) {
+            if (house % i == 0) {
+                factor1_sum += i;
+                if (i * 50 >= house) {
+                    factor2_sum += i;
+                }
+
+                const other_factor = house / i;
+                if (other_factor != i) {
+                    factor1_sum += other_factor;
+                    if (other_factor * 50 >= house) {
+                        factor2_sum += other_factor;
+                    }
+                }
+            }
+        }
+        if (res1 == null and factor1_sum * 10 >= input) {
+            res1 = house;
+        }
+        else if (res2 == null and factor2_sum * 11 >= input) {
+            res2 = house;
+        }
+    }
+
+    return aoc.Solution { .p1 = res1.?, .p2 = res2.? };
+}
--- src/main/zig/2015/runner.zig
+++ src/main/zig/2015/runner.zig
@@ -20,6 +20,7 @@ pub fn run(problem: *aoc.Problem, day: u16) !aoc.Solution {
         17 => @import("day17.zig").run(problem),
         18 => @import("day18.zig").run(problem),
         19 => @import("day19.zig").run(problem),
+        20 => @import("day20.zig").run(problem),
         21 => @import("day21.zig").run(problem),
         22 => @import("day22.zig").run(problem),
         23 => @import("day23.zig").run(problem),
