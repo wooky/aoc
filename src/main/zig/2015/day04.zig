b15b5bf6983871bf37c5aedc9f0e833ba48b4fd8
wjykk <no@no>
2021-12-09 12:57:57 -0700

    Multithread 2015 day 4
    Now runs 10s faster!

--- src/main/zig/2015/day04.zig
+++ src/main/zig/2015/day04.zig
@@ -1,48 +1,40 @@
 const aoc = @import("../aoc.zig");
 const std = @import("std");
 
-
-const HashIterator = struct {
-    const Md5 = std.crypto.hash.Md5;
-    const HashOutput = [Md5.digest_length]u8;
-
-    counter: usize = 0,
-    input: []const u8,
-
-    fn init(input: []const u8) HashIterator {
-        return HashIterator { .input = input };
-    }
-
-    fn next(self: *HashIterator) !?HashOutput {
-        self.counter += 1;
-        var buf: [16]u8 = undefined;
-        const hash_input = try std.fmt.bufPrint(&buf, "{s}{d}", .{self.input, self.counter});
-        var hash_output: HashOutput = undefined;
-        Md5.hash(hash_input, &hash_output, .{});
-        return hash_output;
-    }
+const HashCounter = struct {
+    count: usize = 0,
+    count5: ?usize = null,
+    count6: ?usize = null,
 };
 
 pub fn run(problem: *aoc.Problem) !aoc.Solution {
-    var iterator = HashIterator.init(problem.input);
+    var counter = HashCounter {};
+    var loop: std.event.Loop = undefined;
+    try loop.init();
+    defer loop.deinit();
+    for (loop.extra_threads) |_| {
+        try loop.runDetached(problem.allocator, doHash, .{problem.input, &counter});
+    }
+    loop.run();
 
-    const five = blk: {
-        while (try iterator.next()) |hash| {
-            if (hash[0] == 0 and hash[1] == 0 and hash[2] & 0xF0 == 0) {
-                break :blk iterator.counter;
+    return problem.solution(counter.count5.?, counter.count6.?);
+}
+
+// not thread safe btw
+fn doHash(input: []const u8, counter: *HashCounter) void {
+    var buf: [16]u8 = undefined;
+    var hash: [std.crypto.hash.Md5.digest_length]u8 = undefined;
+    while (counter.count6 == null) {
+        const count = @atomicRmw(usize, &counter.count, .Add, 1, .SeqCst);
+        const hash_input = std.fmt.bufPrint(&buf, "{s}{d}", .{input, count}) catch unreachable;
+        std.crypto.hash.Md5.hash(hash_input, &hash, .{});
+        if (hash[0] == 0 and hash[1] == 0) {
+            if (hash[2] == 0) {
+                counter.count6 = count;
+            }
+            else if (counter.count5 == null and hash[2] & 0xF0 == 0) {
+                counter.count5 = count;
             }
         }
-        unreachable;
-    };
-
-    const six = blk: {
-        while (try iterator.next()) |hash| {
-            if (hash[0] == 0 and hash[1] == 0 and hash[2] == 0) {
-                break :blk iterator.counter;
-            }
-        }
-        unreachable;
-    };
-
-    return problem.solution(five, six);
+    }
 }
