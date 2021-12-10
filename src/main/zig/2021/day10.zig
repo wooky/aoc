3258b1ea4bd0140c6d8460628c6436fbb481e80d
wjykk <no@no>
2021-12-10 00:12:09 -0700

    2021 day 10 in Zig

---
+++ src/main/zig/2021/day10.zig
@@ -0,0 +1,48 @@
+const aoc = @import("../aoc.zig");
+const std = @import("std");
+
+pub fn run(problem: *aoc.Problem) !aoc.Solution {
+    var syntax_error_score: usize = 0;
+    var autocomplete_scores = std.ArrayList(usize).init(problem.allocator);
+    defer autocomplete_scores.deinit();
+    blk: while (problem.line()) |line| {
+        var openers = std.ArrayList(u8).init(problem.allocator);
+        defer openers.deinit();
+        for (line) |c| {
+            switch (c) {
+                ')' => if (openers.pop() != '(') {
+                    syntax_error_score += 3;
+                    continue :blk;
+                },
+                ']' => if (openers.pop() != '[') {
+                    syntax_error_score += 57;
+                    continue :blk;
+                },
+                '}' => if (openers.pop() != '{') {
+                    syntax_error_score += 1197;
+                    continue :blk;
+                },
+                '>' => if (openers.pop() != '<') {
+                    syntax_error_score += 25137;
+                    continue :blk;
+                },
+                else => try openers.append(c)
+            }
+        }
+
+        var score: usize = 0;
+        while (openers.popOrNull()) |c| {
+            score = score * 5 + switch (c) {
+                '(' => @as(u8, 1),
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                else => unreachable,
+            };
+        }
+        try autocomplete_scores.append(score);
+    }
+    std.sort.sort(usize, autocomplete_scores.items, {}, comptime std.sort.asc(usize));
+
+    return problem.solution(syntax_error_score, autocomplete_scores.items[autocomplete_scores.items.len / 2]);
+}
