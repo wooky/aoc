39f1cde4fbcdb62ec65202afb92cd15b2d9975a7
wjykk <no@no>
2022-11-30 23:06:31 -0700

    2022 day 1 in Ada

---
+++ src/main/ada/2022/day01.adb
@@ -0,0 +1,45 @@
+with Ada.Containers; use Ada.Containers;
+with Ada.Containers.Vectors;
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Day01 is
+  package Elves_Calories_Vector is new
+    Ada.Containers.Vectors
+      (Index_Type => Natural,
+       Element_Type => Natural);
+  package Elves_Calories_Vector_Sorting is new Elves_Calories_Vector.Generic_Sorting;
+
+  Elves_Calories : Elves_Calories_Vector.Vector;
+begin
+  declare
+    F : File_Type;
+    Elf_Calories : Natural := 0;
+  begin
+    Open (F, In_File, "input/2022/day01.txt");
+    while not End_Of_File (F) loop
+      declare
+        Line : String := Get_Line (F);
+      begin
+        if Line = "" then
+          Elves_Calories.Append (Elf_Calories);
+          Elf_Calories := 0;
+        else
+          Elf_Calories := Elf_Calories + Natural'Value (Line);
+        end if;
+      end;
+    end loop;
+    Close (F);
+  end;
+
+  Elves_Calories_Vector_Sorting.Sort (Elves_Calories);
+  Put_Line (Elves_Calories.Last_Element'Image);
+  declare
+    Top_3_Calories : Natural := (
+      Elves_Calories.Last_Element +
+      Elves_Calories (Elves_Calories.Last_Index - 1) +
+      Elves_Calories (Elves_Calories.Last_Index - 2)
+    );
+  begin
+    Put_Line (Top_3_Calories'Image);
+  end;
+end Day01;
