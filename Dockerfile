ccfa35048d0781822c6c651fdc6fd0b3e71aa11e
Yakov Lipkovich <460371+wooky@users.noreply.github.com>
2024-12-02 23:46:03 +0000

    Use deno for the runner

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM bitnami/minideb:latest
 
 RUN \
-  install_packages ca-certificates cmake g++ gcc git gnat gprbuild less libc6-dev libgsl-dev libgnatcoll21-dev make nim python3 wget xz-utils && \
-  (wget https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz -O - | tar xJ) && mv zig-linux-x86_64-0.11.0 /usr/local/zig && ln -sf /usr/local/zig/zig /usr/local/bin/zig
+  install_packages ca-certificates cmake curl g++ gcc git gnat gprbuild less libc6-dev libgsl-dev libgnatcoll21-dev make nim unzip wget xz-utils && \
+  (wget https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz -O - | tar xJ) && mv zig-linux-x86_64-0.11.0 /usr/local/zig && ln -sf /usr/local/zig/zig /usr/local/bin/zig && \
+  (wget https://github.com/gleam-lang/gleam/releases/download/v1.6.2/gleam-v1.6.2-x86_64-unknown-linux-musl.tar.gz -O - | tar xz) && mv gleam /usr/local/bin && \
+  (curl -fsSL https://deno.land/install.sh | DENO_INSTALL=/usr/local sh)
+
---
+++ aoc.ts
@@ -0,0 +1,106 @@
+import runners from './runners.json' with { type: 'json' };
+import process from 'node:process';
+
+type Solution = [string, string];
+const extractString = (dv: DataView, offset: number): string => {
+  const ptr = Deno.UnsafePointer.create(dv.getBigUint64(offset, true));
+  if (ptr === null) throw new Error(`Pointer at offset ${offset} is null`);
+  return new Deno.UnsafePointerView(ptr).getCString();
+};
+const extractSolution = (result: Uint8Array): Solution => {
+  const dv = new DataView(result.buffer, result.byteOffset, result.byteLength);
+  return [extractString(dv, 0), extractString(dv, 8)];
+};
+
+const runnerMapping = {
+  "ada": (input: Uint8Array, year: number, day: number): Solution => {
+    const {
+      symbols: {
+        run,
+        aoc_adainit,
+        aoc_adafinal,
+      },
+    } = Deno.dlopen(
+      "build/libaoc_ada.so",
+      {
+        run: {
+          parameters: ["buffer", "u16", "u16"],
+          result: { struct: ["pointer", "pointer"] },
+        },
+        aoc_adainit: {
+          parameters: [],
+          result: "void",
+        },
+        aoc_adafinal: {
+          parameters: [],
+          result: "void",
+        },
+      },
+    );
+
+    aoc_adainit();
+    const solution = extractSolution(run(input, year, day));
+    aoc_adafinal();
+    return solution;
+  },
+
+  "cxx": (input: Uint8Array, year: number, day: number): Solution =>
+    extractSolution(
+      Deno.dlopen(
+        "build/libaoc_cxx.so",
+        {
+          run: {
+            parameters: ["buffer", "u16", "u16"],
+            result: { struct: ["pointer", "pointer"] },
+          },
+        },
+      ).symbols.run(input, year, day),
+    ),
+
+  "nim": (input: Uint8Array, year: number, day: number): Solution =>
+    extractSolution(
+      Deno.dlopen(
+        "build/libaoc_nim.so",
+        {
+          run: {
+            parameters: ["buffer", "u16", "u16"],
+            result: { struct: ["pointer", "pointer"] },
+          },
+        },
+      ).symbols.run(input, year, day),
+    ),
+
+  "zig": (input: Uint8Array, year: number, day: number): Solution =>
+    extractSolution(
+      Deno.dlopen(
+        "build/libaoc_zig.so",
+        {
+          run: {
+            parameters: ["buffer", "u16", "u16"],
+            result: { struct: ["pointer", "pointer"] },
+          },
+        },
+      ).symbols.run(input, year, day),
+    ),
+};
+
+const year = parseInt(process.argv[2]);
+const day = parseInt(process.argv[3]);
+if (isNaN(year) || isNaN(day)) {
+  throw new Error(`Usage: ${process.argv[0]} ${process.argv[1]} [year] [day]`);
+}
+
+let runnerType: string|object = runners[year.toString()];
+if (typeof runnerType === 'object') {
+  runnerType = runnerType[day.toString()];
+}
+if (typeof runnerType !== 'string') {
+  throw new Error('Invalid year/day');
+}
+const runner = runnerMapping[runnerType];
+
+const text = await Deno.readTextFile(`input/${year}/day${day.toString().padStart(2, '0')}.txt`);
+const input = new TextEncoder().encode(`${text}\0`);
+const [s1, s2] = runner(input, year, day);
+console.log(s1);
+console.log(s2);
