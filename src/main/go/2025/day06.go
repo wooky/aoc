97e3b3a47a56e92045e6f184f5fafcac2c1a1793
Yakov Lipkovich <460371+wooky@users.noreply.github.com>
2025-12-06 22:09:22 +0000

    2025 day 6 part 2 in Go

--- src/main/go/2025/day06.go
+++ src/main/go/2025/day06.go
@@ -7,32 +7,55 @@ import (
 )
 
 func day06(input string) (string, string) {
-	s1 := uint64(0)
-	tokenizer := regexp.MustCompile(`\S+`)
+	s1, s2 := uint64(0), uint64(0)
+	tokenizer := regexp.MustCompile(`[+*]\s+`)
 	lines := strings.Split(input, "\n")
-	var numbers [][]uint64
-	for _, line := range lines[:len(lines)-1] {
-		var l []uint64
-		for _, s := range tokenizer.FindAllString(line, -1) {
-			n, _ := strconv.ParseUint(s, 10, 64)
-			l = append(l, n)
+	ops := lines[len(lines)-1]
+
+	for _, fromTo := range tokenizer.FindAllStringIndex(ops, -1) {
+		from, to := fromTo[0], fromTo[1]
+		var horNums, vertNums []uint64
+
+		for _, line := range lines[:len(lines)-1] {
+			num, _ := strconv.ParseUint(strings.TrimSpace(line[from:to]), 10, 64)
+			horNums = append(horNums, num)
 		}
-		numbers = append(numbers, l)
-	}
-	for idx, op := range tokenizer.FindAllString(lines[len(lines)-1], -1) {
-		if op == "+" {
-			res := uint64(0)
-			for _, n := range numbers {
-				res += n[idx]
+
+		for idx := from; idx < to; idx++ {
+			n := uint64(0)
+			for _, line := range lines[:len(lines)-1] {
+				c := line[idx]
+				if c != ' ' {
+					n = n*10 + uint64(c-'0')
+				}
 			}
-			s1 += res
+			if n != 0 {
+				vertNums = append(vertNums, n)
+			}
+		}
+
+		if ops[from] == '+' {
+			r1, r2 := uint64(0), uint64(0)
+			for _, n := range horNums {
+				r1 += n
+			}
+			for _, n := range vertNums {
+				r2 += n
+			}
+			s1 += r1
+			s2 += r2
 		} else {
-			res := uint64(1)
-			for _, n := range numbers {
-				res *= n[idx]
+			r1, r2 := uint64(1), uint64(1)
+			for _, n := range horNums {
+				r1 *= n
 			}
-			s1 += res
+			for _, n := range vertNums {
+				r2 *= n
+			}
+			s1 += r1
+			s2 += r2
 		}
 	}
-	return strconv.FormatUint(s1, 10), ""
+
+	return strconv.FormatUint(s1, 10), strconv.FormatUint(s2, 10)
 }
