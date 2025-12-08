18cb6f79fd1ac918ebddbe8ca3d7342a89a2ea3f
Yakov Lipkovich <460371+wooky@users.noreply.github.com>
2025-12-08 07:58:38 +0000

    2025 day 8 part 2 in Go

--- src/main/go/2025/day08.go
+++ src/main/go/2025/day08.go
@@ -2,7 +2,6 @@ package y2025
 
 import (
 	"maps"
-	"math"
 	"slices"
 	"strconv"
 	"strings"
@@ -28,27 +27,41 @@ func day08(input string) (string, string) {
 		coords = append(coords, Coord3{x, y, z})
 	}
 
-	// Get top closest coord pairs
-	var distances [1000]Distance
-	for i := range distances {
-		distances[i].distance = math.MaxInt
-	}
+	// Get and sort coord pair distances
+	var distances []Distance
 	for i := 0; i < len(coords)-1; i++ {
 		from := coords[i]
 		for _, to := range coords[i+1:] {
 			dx, dy, dz := from.x-to.x, from.y-to.y, from.z-to.z
 			d := dx*dx + dy*dy + dz*dz
-			lastDistance := distances[len(distances)-1]
-			if d <= lastDistance.distance {
-				distances[len(distances)-1] = Distance{d, from, to}
-				slices.SortFunc(distances[:], func(a, b Distance) int {
-					return a.distance - b.distance
-				})
-			}
+			distances = append(distances, Distance{d, from, to})
 		}
 	}
+	slices.SortFunc(distances, func(a, b Distance) int { return a.distance - b.distance })
 
-	// Map each coord to junction box
+	var s1 int
+	{
+		// Map closest 1000 coords to junction box
+		boxes, _ := linkBoxes(distances[:1000], len(coords))
+
+		// Count how many coords exist for each junction box
+		boxCount := make(map[int]int)
+		for _, box := range boxes {
+			boxCount[box]++
+		}
+
+		// Get product of top 3 box counts
+		sortedBoxCounts := slices.Sorted(maps.Values(boxCount))
+		slices.Reverse(sortedBoxCounts)
+		s1 = sortedBoxCounts[0] * sortedBoxCounts[1] * sortedBoxCounts[2]
+	}
+
+	_, s2 := linkBoxes(distances, len(coords))
+
+	return strconv.Itoa(s1), strconv.Itoa(s2)
+}
+
+func linkBoxes(distances []Distance, coordCount int) (map[Coord3]int, int) {
 	boxes := make(map[Coord3]int)
 	nextBox := 0
 	for _, distance := range distances {
@@ -63,26 +76,23 @@ func day08(input string) (string, string) {
 		// Add to coord, or join to's box to from's box
 		toBox, toFound := boxes[distance.to]
 		if toFound && toBox != box {
+			inCircuit := 0
 			for k, v := range boxes {
-				if v == toBox {
+				switch v {
+				case toBox:
 					boxes[k] = box
+					inCircuit++
+				case box:
+					inCircuit++
 				}
+			}
+			if inCircuit == coordCount {
+				return boxes, distance.from.x * distance.to.x
 			}
 		} else {
 			boxes[distance.to] = box
 		}
 	}
 
-	// Count how many coords exist for each junction box
-	boxCount := make(map[int]int)
-	for _, box := range boxes {
-		boxCount[box]++
-	}
-
-	// Get product of top 3 box counts
-	sortedBoxCounts := slices.Sorted(maps.Values(boxCount))
-	slices.Reverse(sortedBoxCounts)
-	s1 := sortedBoxCounts[0] * sortedBoxCounts[1] * sortedBoxCounts[2]
-
-	return strconv.Itoa(s1), ""
+	return boxes, 0
 }
